f7728b3b4e6461d236b8c0f3951544bf1d0632ff
Firas Chabchoub <firaschabchoub@hotmail.com>
2021-10-29 19:38:27 +0100

    refactor: adding LuckuController

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController extends AbstractController
+{
+    /**
+     * @Route("/lucky", name="lucky")
+     */
+    /* public function index(): Response
+    {
+        return $this->render('lucky/index.html.twig', [
+            'controller_name' => 'LuckyController',
+        ]);
+    }
+ */
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+        return new Response("<h1>Lucky number : {$number}</h1>");
+    }
+}
