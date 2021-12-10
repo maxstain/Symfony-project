997c041b26df1da1912a920ef66f85b754264153
Firas Chabchoub <firaschabchoub@hotmail.com>
2021-12-11 00:49:43 +0100

    refactor: working on the CRUD system

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,6 +20,24 @@ class ArticlesController extends AbstractController
         $articles=$articleRepository->findAll();
         return $this->render('articles/index.html.twig', [
             'articles' => $articles,
+        ]);
+    }
+
+    /**
+     * @Route("/articles/add/{designation}/{Description}/{prix}")
+     */
+
+    public function addArticle(EntityManagerInterface $entityManager, string $designation, string $Description, float $prix): Response
+    {
+        $article = new Article();
+        $article->setDesignation($designation);
+        $article->setDescription($Description);
+        $article->setPrix($prix);
+        $entityManager->persist($article);
+        $entityManager->flush();
+        return $this->render('articles/index.html.twig', [
+            'articles' => $article,
+            'adjectif' => 'ajoutée',
         ]);
     }
 }
