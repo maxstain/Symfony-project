09f9a8494d9f091c3754d69e2bf08bf1d39683f5
Firas Chabchoub <firaschabchoub@hotmail.com>
2021-12-12 12:18:58 +0100

    refactor: working on the add article

---
+++ src/Controller/AjoutArticleController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AjoutArticleController extends AbstractController
+{
+    /**
+     * @Route("/articles/add", name="ajout_article")
+     */
+    public function index(): Response
+    {
+        return $this->render('ajout_article/index.html.twig', [
+            'controller_name' => 'AjoutArticleController',
+        ]);
+    }
+}
