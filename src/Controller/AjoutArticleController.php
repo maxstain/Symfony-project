115ce3e8a9ebdc861ef3e8be77604eb12f6c20b0
Firas Chabchoub <firaschabchoub@hotmail.com>
2021-12-24 01:59:32 +0100

    refactor: working on ajout

--- src/Controller/AjoutArticleController.php
+++ src/Controller/AjoutArticleController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\ArticleFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,10 +15,20 @@ class AjoutArticleController extends AbstractController
     /**
      * @Route("/articles/add", name="ajout_article")
      */
-    public function index(): Response
+    public function addArticle(Request $request, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('ajout_article/index.html.twig', [
+        $article = new Article();
+        $form = $this->createForm(ArticleFormType::class, $article);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article = $form->getData();
+            $entityManager->persist($article);
+            $entityManager->flush();
+            return $this->redirectToRoute('articles_show_all');
+        }
+        return $this->render('article/form.html.twig', [
             'controller_name' => 'Ajout article',
+            'form' => $form->createView(),
         ]);
     }
 }
